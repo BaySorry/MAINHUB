fa0671fd07c6ef302aac959e1bb11e8dcbf30573
MORND <76564648+BaySorry@users.noreply.github.com>
2026-04-05 17:05:34 +0300

    Update Wsup.lua

--- Update Checker/Wsup.lua
+++ Update Checker/Wsup.lua
@@ -74,7 +74,7 @@ local Database = {
     ["70845479499574"] = {
         Name = "Bite By Night",
         ScriptUrl = "https://raw.githubusercontent.com/BaySorry/MAINHUB/refs/heads/main/BiteByNight/BiteByNight.lua",
-        NeedsUpdate = false
+        NeedsUpdate = true
     },
 }
 
